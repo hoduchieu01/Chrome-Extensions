7f46899dc9a235426fd06c04613427a3319e5318
Hồ Đức Hiếu <hoduchieu01@gmail.com>
2018-06-07 03:06:10 +0700

    add speak extensions

---
+++ Speak/eventPage.js
@@ -0,0 +1,14 @@
+var menuItem = {
+    "id": "Speak",
+    "title": "Speak",
+    "contexts": ["selection"]
+};
+chrome.contextMenus.create(menuItem);
+chrome.contextMenus.onClicked.addListener(function(clickData){
+    if (clickData.menuItemId == "Speak" && clickData.selectionText){
+       chrome.tts.speak(clickData.selectionText,
+                        {
+                            'rate': 0.6,
+                        });
+    }
+});
